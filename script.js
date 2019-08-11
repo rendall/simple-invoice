8afbe7ad8242de0f382f51ccd2c5e76ef1a6ea96
Rendall <rendall@gmail.com>
2019-08-11 17:44:04 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,48 @@
+const save = (e) => {
+  const keyName = e.target.classList[0]
+  const value = e.target.innerText
+  const rowIndex = (!!e.target.parentElement.rowIndex) ? e.target.parentElement.rowIndex : 0
+  const key = rowIndex === 0 ? keyName : `item-${rowIndex}__${keyName}`
+  document.cookie = `${key}=${value}`
+}
+
+const calculate = () => {
+  const items = Array.from(document.querySelectorAll("tr.item"))
+  const parseValue = (str) => parseFloat(str.replace(",", ".").replace(/[^\.\d]/g, ""))
+  const getValue = (tr, index) => parseValue(tr.children[index].innerText)
+  const getQuantity = (tr) => getValue(tr, 1)
+  const getPrice = (tr) => getValue(tr, 2)
+  const subtotal = items.reduce((sum, row) => getQuantity(row) * getPrice(row) + sum, 0)
+  const vat = subtotal * 0.24
+  const total = subtotal * 1.24
+  const addGap = (str) => str.slice(0, -6) + " " + str.slice(-6)
+  const formatValue = (x) => addGap(x.toFixed(2)).replace(".", ",") + "€"
+  document.querySelector(".subtotal").children[1].innerText = formatValue(subtotal)
+  document.querySelector(".taxes").children[1].innerText = formatValue(vat)
+  document.querySelector(".total").children[1].innerText = formatValue(total)
+}
+
+const populateInvoice = () => {
+  const keyValues = document.cookie.split(';').map(c => c.trim())
+  const info = keyValues.filter(kv => !/^item-\d?/.test(kv))
+  info.filter(i => i.length > 0).forEach(i => {
+    const kv = i.split('=')
+    document.querySelector(`.${kv[0]}`).innerText = kv[1]
+    if (kv[0]==="company-name") document.title = kv[1]
+  })
+  if (document.querySelector(".invoice-created__value").innerText.trim() === "") document.querySelector(".invoice-created__value").innerText = new Date().toLocaleDateString();
+  const dueIn = 2505600000; // 30 days of time in milliseconds 
+  if (document.querySelector(".invoice-due__value").innerText.trim() === "") document.querySelector(".invoice-due__value").innerText = new Date(Date.now() + dueIn).toLocaleDateString();
+}
+
+const initialize = () => {
+  const editables = Array.from(document.querySelectorAll("[contenteditable]"))
+  editables.forEach(e => e.addEventListener("input", save))
+  const tableValues = Array.from(document.querySelectorAll("table.itemization .item [contenteditable]"))
+  tableValues.forEach(e => e.addEventListener("input", calculate))
+  populateInvoice()
+  calculate()
+  editables[0].focus()
+}
+
+initialize()
